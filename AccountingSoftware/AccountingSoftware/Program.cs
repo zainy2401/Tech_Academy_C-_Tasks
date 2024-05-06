fb1bbe28a235a9f4df396f4eb3fa3be7c9676c2f
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-06 13:20:09 +0100

    Add files via upload

---
+++ AccountingSoftware/AccountingSoftware/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Progrma { 
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College. \nStudent Daily Report"); //Prints a string of words to the console. A header for the program.
+            Console.WriteLine("What is your name?"); //Prints a string of words to the console. A question.
+            string name = Console.ReadLine(); //Creates variable and stores user input as string.
+
+            Console.WriteLine("What Course are you on?"); //Prints a string of words to the console. A question.
+            string coursName = Console.ReadLine(); //Creates variable and stores user input as string.
+
+            Console.WriteLine("What page number?"); //Prints a string of words to the console. A question.
+            int pageNumber = Convert.ToInt32(Console.ReadLine()); //Creates variable and stores user input as integer by being converted first.
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true or false\" "); //Prints a string of words to the console. A question.
+            bool needHelp = Convert.ToBoolean(Console.ReadLine()); //Creates variable and stores user input as boolean by being converted first.
+
+            Console.WriteLine("Was there any positive experiences you'd like to share? Please give specifics"); //Prints a string of words to the console. A question.
+            string experience = Console.ReadLine(); //Creates variable and stores user input as string.
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific"); //Prints a string of words to the console. A question.
+            string feedback = Console.ReadLine(); //Creates variable and stores user input as string.
+
+            Console.WriteLine("How many hours did you study today?"); //Prints a string of words to the console. A question.
+            int hoursStudied = Convert.ToInt32(Console.ReadLine()); //Creates variable and stores user input as integer by being converted first.
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond shortly. Have a great day!"); //Prints a string of words to the console. A closing statement.
+        }
+    }
+}
