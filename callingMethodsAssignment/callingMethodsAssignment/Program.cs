a5c057898a5d3cc80462c8b50de26ba3b4248990
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-22 12:24:32 +0100

    Add files via upload

---
+++ callingMethodsAssignment/callingMethodsAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace callingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task newTask = new Task();
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input, " + input + ", added by 2 is " + newTask.Add(input));
+            Console.WriteLine("Your input, " + input + ", subtracted by 2 is " + newTask.Subtract(input));
+            Console.WriteLine("Your input, " + input + ", multiplied by 2 is " + newTask.Multiply(input));
+        }
+    }
+}
---
+++ callingMethodsAssignment/callingMethodsAssignment/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace callingMethodsAssignment
+{
+    class Task
+    {
+        public int Add(int input)
+        {
+            int result = input + 2;
+            return result;
+        }
+
+        public int Subtract(int input)
+        {
+            int result = input - 2;
+            return result;
+        }
+
+        public int Multiply(int input)
+        {
+            int result = input * 2;
+            return result;
+        }
+    }
+}
