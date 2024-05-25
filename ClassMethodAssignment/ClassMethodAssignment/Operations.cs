839dd2c35c4ee5e6c167a1955493d836f3f760fd
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-25 02:01:44 +0100

    Add files via upload

---
+++ ClassMethodAssignment/ClassMethodAssignment/Operations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodAssignment
+{
+    public class Operations
+    {
+        public void divide(int num1)
+        {
+            Console.WriteLine(num1 + " divided by 2 is " + (num1 / 2));
+        }
+
+        public void output()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void output(string input)
+        {
+            Console.WriteLine("Your message was: " + input);
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operation = new Operations();
+            Console.WriteLine("Please enter a number to divide by 2: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            operation.divide(input);
+            operation.output();
+            Console.WriteLine("Please enter something to output: ");
+            string input2 = Console.ReadLine();
+            operation.output(input2);
+            
+        }
+    }
+}
