1b50a44abf28ee501b9b7dd7c053a3dde9fef707
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-25 01:06:26 +0100

    Add files via upload

---
+++ MainMethodAssignment/MainMethodAssignment/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class Operations
+    {
+        public int addition(int input)
+        {
+            int result = input + 5;
+            return result;
+        }
+        public int addition(float input)
+        {
+            int result = (int)(input + 5);
+            return result;
+        }
+        public int addition(string input)
+        {
+            int result = Convert.ToInt32(input) + 5;
+            return result;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations test = new Operations();
+            Console.WriteLine(test.addition(5));
+            Console.WriteLine(test.addition(4.5f));
+            Console.WriteLine(test.addition("10"));
+        }
+    }
+}
