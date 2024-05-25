d4cda1c053fea698f98a0644a41e55979efd2856
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-25 01:24:26 +0100

    Add files via upload

---
+++ MethodAssignment/MethodAssignment/Operations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment
+{
+    public class Operations
+    {
+        public int addition(int input, int optional = 5)
+        {
+            int result = input + optional;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations test = new Operations();
+            Console.WriteLine("Please enter a first Number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optionally enter a second number or just press enter to skip: ");
+            try
+            {
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(test.addition(firstNumber, secondNumber));
+            }
+            catch
+            {
+                Console.WriteLine(test.addition(firstNumber));
+            }
+        }
+    }
+}
