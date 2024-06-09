84328fb20b836f0bd99d2655164660cf81ddec3b
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-09 18:13:27 +0100

    Add files via upload

---
+++ StructAssignment/StructAssignment/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; } //Creates a decimal variable
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number() { Amount = 6.9m }; //Creates and initalises an object of type Number struct, making it a value type
+            Console.WriteLine(num1.Amount); //Outputs the value of the property of the object
+        }
+    }
+}
