baa812dda30d1339f5913710ebbb958aad53481c
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-13 20:42:08 +0100

    Add files via upload

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current date is: ");
+            Console.Write(DateTime.Now);
+            Console.WriteLine("Please enter in a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours, the time will be {1}", input, (DateTime.Now.AddHours(input)));
+        }
+    }
+}
