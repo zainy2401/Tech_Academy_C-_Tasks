6ca8e28bb61ba06ba49ac98fcb7c292f295c629d
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-13 20:25:01 +0100

    Add files via upload

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\zaiqu\Desktop\InputAssignment.txt", true))
+            {
+                file.WriteLine(input);
+            }
+            Console.WriteLine("Here is what is in the text file: ");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\zaiqu\Desktop\InputAssignment.txt"));
+        }
+    }
+}
