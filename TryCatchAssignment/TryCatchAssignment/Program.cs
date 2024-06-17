daf195e46b2ac9eaa51ab2512e464076e2b613a2
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-17 20:30:32 +0100

    Add files via upload

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if(age <= 0)
+                {
+                    throw new ArgumentException("Age must not be 0 or negative");
+                }
+                DateTime currentDate = DateTime.Now;
+                int yearOfBirth = currentDate.Year - age;
+                Console.WriteLine(yearOfBirth);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unknown error ocurred, please contact system administrator.");
+            }
+        }
+    }
+}
