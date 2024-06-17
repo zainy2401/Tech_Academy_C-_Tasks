a8fc1b05151308667f86ded1112001de33f4d96d
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-17 19:49:41 +0100

    Add files via upload

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        public Person(string name) : this(name, 23)
+        {
+            this.name = name;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Zain";
+            var age = 22;
+
+            Person person = new Person(name, age);
+            Console.WriteLine("Person 1 Name: {0}, Age: {1}", person.name, person.age);
+
+            Person person2 = new Person("Bob");
+            Console.WriteLine("Person 1 Name: {0}, Age: {1}", person2.name, person2.age);
+        }
+    }
+}
